8bcb778fdaaedb05b539c2254f922714ce9ad089
nicknish09 <nnmarques97@gmail.com>
2019-08-23 19:41:33 -0300

    criado componente de timer

--- src/views/Timetracker/components/InputBar.js
+++ src/views/Timetracker/components/InputBar.js
@@ -1,56 +1,16 @@
-import React, { useState } from 'react';
-import Timer from "react-compound-timer";
-import TimeField from 'react-simple-timefield';
+import React, { useState, useEffect } from 'react';
 import DivFlex from '../../../components/DivFlex';
+import Timer from "./Timer";
 
 function InputBar() {
   // Declare uma nova variável de state, a qual chamaremos de "count"
-  const [time, setTime] = useState('00:00');
-  const [timeRunning, setTimeRunning] = useState(false);
-
-  function onTimeChange(time) {
-    setTime(time);
-  }
 
   return (
     <div className={'card'}>
       <div className={'card-body'}>
         <DivFlex>
           <input type="text" name="timeentry" className={'form-control'}/>
-          <Timer
-            initialTime={10000}
-            startImmediately={false}
-            onStart={() => setTimeRunning(true)}
-            onResume={() => console.log('onResume hook')}
-            onPause={() => console.log('onPause hook')}
-            onStop={() => setTimeRunning(false)}
-            onReset={() => console.log('onReset hook')}
-          >
-            {
-              ({ start, resume, pause, stop, reset, timerState }) => (
-              <React.Fragment>
-                <div>
-                  <TimeField
-                    value={time}
-                    onChange={onTimeChange}
-                    style={{marginRight: 20, marginLeft: 20, width: 80}}
-                    showSeconds={true}
-                    input={<input type="text" className="form-control"/>}
-                  />
-                </div>
-                <br />
-                <div>
-                  {timeRunning?
-                    <button onClick={stop} className={'btn btn-danger'}>Stop</button>
-                    : <button onClick={start} className={'btn btn-primary'}>Start</button>}
-                  {/*<button onClick={pause}>Pause</button>*/}
-                  {/*<button onClick={resume}>Resume</button>*/}
-                  {/*<button onClick={reset}>Reset</button>*/}
-                </div>
-              </React.Fragment>
-              )
-            }
-          </Timer>
+          <Timer/>
         </DivFlex>
       </div>
     </div>
---
+++ src/views/Timetracker/components/Timer.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import TimeField from 'react-simple-timefield';
+import DivFlex from '../../../components/DivFlex';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(1);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState('00:00');
+
+  function onTimeChange(time) {
+    setTime(time);
+  }
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    let h, m, s;
+    if (isActive) {
+      interval = setInterval( () => {
+        if (seconds == 59) { setMinutes(minutes => minutes + 1); setSeconds(-1); }
+        if (minutes == 59 && seconds == 59) { setHours(hours => hours + 1); setSeconds(-1); setMinutes(0); }
+        if(hours < 10){
+          h = `0${hours}`;
+        } else {
+          h = hours;
+        }
+        if(minutes < 10){
+          m = `0${minutes}`;
+        } else {
+          m = minutes;
+        }
+        if(seconds < 10){
+          s = `0${seconds}`;
+        } else {
+          s = seconds;
+        }
+        setTime(`${h}:${m}:${s}`);
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <DivFlex>
+      <TimeField
+        value={time}
+        onChange={onTimeChange}
+        style={{marginRight: 20, marginLeft: 20, width: 100}}
+        showSeconds={true}
+        input={<input type="text" className="form-control text-center"/>}
+      />
+      <div className="row">
+        {isActive ?
+          <div>
+            <button className={"btn btn-warning"} onClick={toggle}>
+              Pause
+            </button>
+            <button className="btn btn-danger" onClick={reset}>
+              Stop
+            </button>
+          </div> :
+          <button className={"btn btn-primary"} onClick={toggle}>
+            Start
+          </button>
+        }
+      </div>
+    </DivFlex>
+  );
+};
+
+export default Timer;
