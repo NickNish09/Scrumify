7623c57e066886471c8c057e05cfadabfce06f0c
nicknish09 <nnmarques97@gmail.com>
2019-08-23 09:25:50 -0300

    Primeiro Commit

---
+++ src/_nav.js
@@ -0,0 +1,128 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+    },
+    {
+      title: true,
+      name: 'Kanban',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Kanban',
+      url: '/theme/colors',
+      icon: 'icon-list',
+    },
+    {
+      name: 'Typography',
+      url: '/theme/typography',
+      icon: 'icon-pencil',
+    },
+    {
+      title: true,
+      name: 'Horas',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Time Tracker',
+      url: '/base',
+      icon: 'icon-puzzle',
+      children: [
+        {
+          name: 'Breadcrumbs',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Cards',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Carousels',
+          url: '/base/carousels',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Collapses',
+          url: '/base/collapses',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Dropdowns',
+          url: '/base/dropdowns',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Forms',
+          url: '/base/forms',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Jumbotrons',
+          url: '/base/jumbotrons',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'List groups',
+          url: '/base/list-groups',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Navs',
+          url: '/base/navs',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Paginations',
+          url: '/base/paginations',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Popovers',
+          url: '/base/popovers',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Progress Bar',
+          url: '/base/progress-bar',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Switches',
+          url: '/base/switches',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Tables',
+          url: '/base/tables',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Tabs',
+          url: '/base/tabs',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Tooltips',
+          url: '/base/tooltips',
+          icon: 'icon-puzzle',
+        },
+      ],
+    },
+    {
+      name: 'Charts',
+      url: '/charts',
+      icon: 'icon-pie-chart',
+    },
+  ],
+};
